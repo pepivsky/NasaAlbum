b20328f9f9c9e6a5317c167f247c26c44a9fb54d
pepivsky <ironglas68@gmail.com>
2024-09-09 20:04:21 -0600

    padding in ads banner

--- app/src/main/java/com/pepivsky/NasaScreen.kt
+++ app/src/main/java/com/pepivsky/NasaScreen.kt
@@ -43,7 +43,7 @@ fun NasaScreen(viewModel: NasaScreenViewModel, navigationController: NavHostCont
             is HomeUiState.Success -> {
                 Column(modifier = Modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally) {
                     Header()
-                    AdvertView(modifier = Modifier.size(width = 320.dp, height = 50.dp))
+                    AdvertView(modifier = Modifier.size(width = 320.dp, height = 60.dp).padding(bottom = 24.dp))
                     PhotosGrid(viewModel, navigationController)
                 }
             }
